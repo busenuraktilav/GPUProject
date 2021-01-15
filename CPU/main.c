e4ec468783dc377a4a3ba0bb1c81c4e5cd1a24c3
buse <busenuraktilav@gmail.com>
2021-01-15 14:59:39 +0300

    updates on files

--- CPU/main.c
+++ CPU/main.c
@@ -9,101 +9,167 @@
 #include "heap.h"
 #include "../GPU/bellman_ford.cuh"
 #include "../GPU/dijkstra.cuh"
+#include "../GPU/hybrid.cuh"
 //#include "cpu_utils.h" //not finished. will be added later
 
 
 
 int main(int argc, char const *argv[])
 {
-	//const char* file = "../example_graphs/graph_10_16_pos-10.txt";
-	//const char* file = "../example_graphs/deneme.txt";
-	const char* file = "../example_graphs/Trefethen_2000.mtx";
+	const char* file = "../example_graphs/graph_13_16_pos-10.txt";
+	//const char* file = "../example_graphs/article.txt";
+	//const char* file = "../example_graphs/Trefethen_2000.mtx";
+	//const char* file = "../example_graphs/graph_18_16_mix-1-100.txt";
 	
-	int start = 1;
+	int start;
 
-	int *row_ptr, *col_ind, *row_ind, *weights, *cpu_bf_distance, *gpu_bf_distance, *cpu_dj_distance, *gpu_dj_distance, 
-	    *cpu_hybrid_distance, *gpu_hybrid_distance, *cpu_bf_previous, *gpu_bf_previous, *cpu_dj_previous, *gpu_dj_previous, 
-	    *cpu_hybrid_previous, *gpu_hybrid_previous, *visited, nv, ne;
+	int *row_ptr, *col_ind, *row_ind, *weights, nv, ne, neg_edge_count = 0;
+	int *cpu_bf_distance, *cpu_bf_previous, *gpu_bf_distance, *gpu_bf_previous;
+	int *cpu_dj_distance, *cpu_dj_previous, *gpu_dj_distance, *gpu_dj_previous;
+	int *cpu_hybrid_distance, *cpu_hybrid_previous, *gpu_hybrid_distance, *gpu_hybrid_previous;
 
-	int read = read_graph(file, &row_ptr, &col_ind, &row_ind, &weights, &nv, &ne);
+	int read = read_graph(file, &row_ptr, &col_ind, &row_ind, &weights, &nv, &ne, &neg_edge_count);
 
 
 	if(read == 1)
 	{
+
+		int start = 0;
+		int max_degree = 0, deg;
+		for (int i = 0; i < nv; ++i)
+		{
+			deg = row_ptr[i + 1] - row_ptr[i];
+			if (deg > max_degree)
+			{
+				start = i;
+				max_degree = deg;
+			}
+		}
+
+		printf("Start node: %i\n", start);
+
+		
 		clock_t strt, end;
+		int cnt = 0;
+	
+		
+		//BELLMAN-FORD CPU codes run
 
-		printf("before bellman_ford\n");
+		int num = 0;
 
-		//CPU codes run
 		strt = clock();
 		bellman_ford(&row_ptr, &col_ind, &row_ind, &weights, &cpu_bf_distance, &cpu_bf_previous, nv, ne, start);
 		end = clock();
 		double cpu_bf = ((double) (end - strt)) / CLOCKS_PER_SEC;
 
-		printf("after bellman_ford\n");
+		printf("BELLMAN-FORD CPU TIME (s): %f\n", cpu_bf);
+	
+		
+		//BELLMAN-FORD GPU
 
+		sbf(row_ptr, col_ind, row_ind, weights, &gpu_bf_distance, &gpu_bf_previous, nv, ne, start);
+	
+
+		//DIJKSTRA GPU ALGORITHM
+
+		int count;
+		double gpu_dj = 0;
+
+		sdj(row_ptr, col_ind, weights, &gpu_dj_distance, &gpu_dj_previous, nv, ne, start, &count, &gpu_dj);
+	
+
+		//DIJKSTRA CPU
+			
 		strt = clock();
-		dijkstra(&row_ptr, &col_ind, &row_ind, &weights, &cpu_dj_distance, &cpu_dj_previous, nv, ne, start);
+		dijkstra(&row_ptr, &col_ind, &row_ind, &weights, &cpu_dj_distance, &cpu_dj_previous, nv, ne, start, &count);
 		end = clock();
+	
 		double cpu_dj = ((double) (end - strt)) / CLOCKS_PER_SEC;
 
-		printf("after dijkstra\n");
+		printf("DIJKSTRA CPU TIME (s): %f\n", cpu_dj);
+		
 
+		//HYBRID CPU CODE
+
+		printf("neg_edge_count: %i\n", neg_edge_count);
+		
 		strt = clock();
-		hybrid(&row_ptr, &col_ind, &row_ind, &weights, &cpu_hybrid_distance, &cpu_hybrid_previous, nv, ne, start);
+		hybrid(&row_ptr, &col_ind, &row_ind, &weights, &cpu_hybrid_distance, &cpu_hybrid_previous, nv, ne, start, &count, neg_edge_count);
 		end = clock();
+
 		double cpu_hybrid = ((double) (end - strt)) / CLOCKS_PER_SEC;
 
-		printf("after hybrid\n");
+		printf("HYBRID CPU TIME (s): %f\n", cpu_hybrid);
+		
 
-		printf("CPU_BF: %f\nCPU_DJ: %f\nCPU_HYBRID: %f\n", cpu_bf, cpu_dj, cpu_hybrid);
+		//HYBRID GPU CODE
 
-
-		//GPU codes run
-		sbf(row_ptr, col_ind, row_ind, weights, &gpu_bf_distance, &gpu_bf_previous, nv, ne, start);
-		sdj(row_ptr, col_ind, weights, &gpu_dj_distance, &gpu_dj_previous, nv, ne, start);
+		shybrid(row_ptr, col_ind, weights, &gpu_hybrid_distance, &gpu_hybrid_previous, nv, ne, start, &count, neg_edge_count);
 
 
-		//distance check. write a func later
-
+		/*
 		int check = 0;
-		int count = 0;
+		count = 0;
+
+		printf("OK\n");
+
 
 		for(int i = 0; i < nv; i++)
 		{
-			//if(cpu_dj_distance[i] != gpu_dj_distance[i])
-			if(cpu_bf_distance[i] != gpu_bf_distance[i])
+			if(cpu_dj_distance[i] != gpu_dj_distance[i])
+			//if(cpu_bf_distance[i] != gpu_bf_distance[i])
+
+			//if(gpu_bf_distance[i] != gpu_hybrid_distance[i])
+			//if(cpu_bf_distance[i] != gpu_hybrid_distance[i])
 			{
 				check = 1;
-				//count++;
+				count++;
+				//printf("OK_IF\n");
 				//printf("cpu_bf_distance[%i]: %i, gpu_bf_distance[%i]: %i\n", i, cpu_bf_distance[i], i, gpu_bf_distance[i]);
-				//printf("cpu_dj_distance[%i]: %i, gpu_dj_distance[%i]: %i\n", i, cpu_dj_distance[i], i, gpu_dj_distance[i]);
+				printf("cpu_dj_distance[%i]: %i, gpu_dj_distance[%i]: %i\n", i, cpu_dj_distance[i], i, gpu_dj_distance[i]);
+				//printf("gpu_bf_distance[%i]: %i, gpu_shybrid_distance[%i]: %i\n", i, gpu_bf_distance[i], i, gpu_hybrid_distance[i]);
+				//printf("cpu_bf_distance[%i]: %i, gpu_shybrid_distance[%i]: %i\n", i, cpu_bf_distance[i], i, gpu_hybrid_distance[i]);
 				//break;
 			}
 			else
 			{
+				//printf("OK_ELSE\n");
 				//printf("cpu_bf_distance[%i]: %i, gpu_bf_distance[%i]: %i\n", i, cpu_bf_distance[i], i, gpu_bf_distance[i]);
 				//printf("cpu_dj_distance[%i]: %i, gpu_dj_distance[%i]: %i\n", i, cpu_dj_distance[i], i, gpu_dj_distance[i]);
+				//printf("cpu_bf_distance[%i]: %i, gpu_shybrid_distance[%i]: %i\n", i, cpu_bf_distance[i], i, gpu_hybrid_distance[i]);
 			}
+			//printf("inside for\n");
 		}
+		
+
+		//printf("last ok\n");
 
 		printf("check: %i, count: %i, nv: %i\n", check, count, nv);
+		
+		*/
 
+		
 		
 		free(row_ptr);
 		free(col_ind);
 		free(row_ind);
 		free(weights);
-		//free(cpu_bf_distance);
+		
+
+
+		free(cpu_bf_distance);
 		free(cpu_dj_distance);
 		//free(cpu_bf_previous);
-		free(cpu_dj_previous);
+		//free(cpu_dj_previous);
+		
 		free(gpu_bf_distance);
 		free(gpu_dj_distance);
-		free(gpu_bf_previous);
-		free(gpu_dj_previous);
+		//free(gpu_bf_previous);
+		//free(gpu_dj_previous);
+		
 		
 	}
+
 
 	return 0;
 }
--- GPU/bellman_ford.cu
+++ GPU/bellman_ford.cu
@@ -4,11 +4,13 @@ extern "C" {
 }
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+#include <iostream>
+#include <stdbool.h>
 #include <cuda.h>
 #include <cuda_profiler_api.h>
 
-#define INF 9999
 #define N_THREADS_PER_BLOCK 1024
 
 
@@ -20,24 +22,55 @@ extern "C" {
     	exit(1); \
  }
 
-__global__ void cudasbf(int *row_ptr, int *col_ind, int *row_ind, int *weights, int *distance, int *previous, int nv, int ne)
+__global__ void relax(int *row_ptr, int *col_ind, int *weights, int *queue, int *nextQueue, int size, int* nextSize, int* nv, int* distance) 
 {
-	const int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-	for(int v = 0; v < nv; v++)
-	{
-		if(tid < ne)
-		{
-			//printf("%i\n", tid);
-			int tempdistance = distance[(row_ind)[tid]] + weights[tid];
+    int index, u, v, w, du, dv, add, tid = threadIdx.x + (blockDim.x * blockIdx.x);
 
-			if(tempdistance < distance[(col_ind)[tid]] && distance[(row_ind)[tid]] != INT_MAX)
-			{
-				distance[(col_ind)[tid]] = tempdistance;
-				previous[(col_ind)[tid]] = row_ind[tid];
-			}
-		}
-	}
+    if (tid < size) { 
+        
+    	u = queue[tid];
+    	//printf("queue[%i]: %i\n", tid, queue[tid]);
+    	du = distance[u];
+
+    	//printf("row_ptr[%i]: %i, row_ptr[%i]: %i\n", u, row_ptr[u], u+1, row_ptr[u+1]);
+
+        for (int e = row_ptr[u]; e < row_ptr[u + 1]; e++) {
+            
+            v = col_ind[e];
+            w = weights[e];
+            dv = distance[v];
+            add = du + w;
+            //printf("tid:%i, u: %i, du: %i, v: %i, w: %i, dv: %i, add:%i\n",tid, u, du, v, w, dv,add );
+
+            if (du == INT_MAX){
+                add = INT_MAX;
+            }
+
+            if (add < dv)
+            {
+            	atomicMin(&distance[v],add);
+            	index = atomicAdd(nextSize, 1);
+				nextQueue[index] = v;
+            }
+
+        }
+    }
+
+    if(tid == size)
+    {
+
+    }
+}
+
+
+int
+compare_ints (const void *a, const void *b)
+{
+  const int *da = (const int *) a;
+  const int *db = (const int *) b;
+
+  return (int)(*da > *db) - (*da < *db);
 }
 
 
@@ -45,12 +78,16 @@ extern "C"
 
 void sbf(const int *row_ptr, const int *col_ind, const int *row_ind, const int *weights, int **distance, int **previous, const int nv, const int ne, int source)
 {
+
 	// Initialize GPU variables
 	int *d_row_ptr, *d_col_ind, *d_row_ind, *d_weights, *d_distance, *d_previous, *d_nv, *d_ne;
+	int *d_queue, *d_nextQueue, *d_nextSize;
 	
 	// Initialize CPU variables
 	*distance = (int*)malloc(nv*sizeof(int)); 
 	*previous = (int*)malloc(nv*sizeof(int));
+	int *visited = (int *)calloc(nv, sizeof(int));
+
 
 	for (int i = 0; i < nv; i++)
 	{
@@ -59,66 +96,145 @@ void sbf(const int *row_ptr, const int *col_ind, const int *row_ind, const int *
 	}
 
 	(*distance)[source] = 0;
+	visited[source] = 1;
 
 
-	// Allocate device
-	cudaMalloc((void **)&d_row_ptr, (nv+1)*sizeof(int));
-	cudaMalloc((void **)&d_col_ind, (ne+1)*sizeof(int));
-	cudaMalloc((void **)&d_row_ind, (ne+1)*sizeof(int));
-	cudaMalloc((void **)&d_weights, (ne+1)*sizeof(int));
-	cudaMalloc((void **)&d_distance, nv*sizeof(int));
-	cudaMalloc((void **)&d_previous, nv*sizeof(int));
-	cudaMalloc((void **)&d_nv, sizeof(int));
-	cudaMalloc((void **)&d_ne, sizeof(int));
+	//To increase parallelism, first process the source vertex
+	int srcNeigh = row_ptr[source + 1] - row_ptr[source];
+	int *srcArr = (int*)calloc(srcNeigh, sizeof(int));
+	int index = 0;
+	(*distance)[source] = 0;
 
-	//Copy inputs to device
-	cudaMemcpy(d_row_ptr, row_ptr, (nv+1)*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_col_ind, col_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_row_ind, row_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_weights, weights, (ne+1)*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_distance, (*distance), nv*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_nv, &nv, sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_ne, &ne, sizeof(int), cudaMemcpyHostToDevice);
+	for (int i = row_ptr[source]; i < row_ptr[source + 1]; i++) { // for each neighbor of source node
+		
+		int w = weights[i];
+		int du = (*distance)[source];
+		int dv = (*distance)[col_ind[i]];
 
-
-	cudaEvent_t start;
-	cudaEvent_t stop;
-
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord(start, 0);
-
-	cudasbf<<<(ne+1023)/1024, 1024>>>(d_row_ptr, d_col_ind, d_row_ind, d_weights, d_distance, d_previous, nv, ne);
-
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-	float elapsed;
-	cudaEventElapsedTime(&elapsed, start, stop);
-
-	// Copy outputs to host
-	cudaMemcpy((*distance), d_distance, nv*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy((*previous), d_previous, nv*sizeof(int), cudaMemcpyDeviceToHost);
-
-
-	// check for negative cycles
-	for(int e = 0; e < ne; e++)
-	{
-		if((*distance)[row_ind[e]] + weights[e] < (*distance)[(col_ind)[e]] && (*distance)[row_ind[e]] != INT_MAX)
+		if (du + w < dv && du != INT_MAX)
 		{
-			printf("cuda Error: negative cycle exists\n");
-			break;
+			(*distance)[col_ind[i]] = du + w;
+			srcArr[index++] = col_ind[i]; // add to frontier
+			//printf("col_ind[i]: %i\n", col_ind[i]);
 		}
 	}
 
 
+	// Allocate device
+	cudaCheck(cudaMalloc((void **)&d_row_ptr, (nv+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_col_ind, (ne+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_row_ind, (ne+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_weights, (ne+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_distance, nv*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_previous, nv*sizeof(int)));
+	cudaCheck(cudaMalloc((void**) &d_nextSize, sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_nv, sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_ne, sizeof(int)));
+	cudaCheck(cudaMalloc((void **) &d_queue, (nv+1)*sizeof(int)));
+    cudaCheck(cudaMalloc((void **) &d_nextQueue, (nv+1)*sizeof(int)));
+    cudaCheck(cudaMalloc((void **) &d_nextSize, sizeof(int)));
+
+	//Copy inputs to device
+	cudaCheck(cudaMemcpy(d_row_ptr, row_ptr, (nv+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_col_ind, col_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_row_ind, row_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_weights, weights, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_distance, (*distance), nv*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_nv, &nv, sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_ne, &ne, sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_queue, srcArr, srcNeigh * sizeof(int), cudaMemcpyHostToDevice));
+
+
+	int size = srcNeigh;
+	int *nextSize = (int*)malloc(sizeof(int));
+	*nextSize = 0;
+	
+	cudaEvent_t start;
+	cudaEvent_t stop;
+	cudaCheck(cudaEventCreate(&start));
+	cudaCheck(cudaEventCreate(&stop));
+	cudaCheck(cudaEventRecord(start, 0));
+
+	int round = 1;
+
+	cudaProfilerStart();
+
+	int temp = 0;
+
+	while((size > 0) && (round < nv)) { temp += size;
+
+		cudaCheck(cudaMemcpy(d_nextSize, nextSize, sizeof(int), cudaMemcpyHostToDevice));
+
+		relax<<<(size + 1023) / 1024, 1024>>>(d_row_ptr, d_col_ind, d_weights, d_queue, d_nextQueue, size, d_nextSize, d_nv, d_distance);
+
+		cudaCheck(cudaMemcpy(nextSize, d_nextSize, sizeof(int), cudaMemcpyDeviceToHost));
+
+		printf("size: %i\n", size);
+
+		size = *nextSize;
+		*nextSize = 0;
+		std::swap(d_queue, d_nextQueue); // swap frontiers
+
+		//printf("round: %i\n", round);
+
+		round++;
+
+	}
+
+	printf("total size: %i\n", temp);
+
+	cudaProfilerStop();
+
+	cudaCheck(cudaEventRecord(stop, 0));
+	cudaCheck(cudaEventSynchronize(stop));
+	float elapsed;
+	cudaCheck(cudaEventElapsedTime(&elapsed, start, stop));
+
+	// Copy outputs to host
+	cudaCheck(cudaMemcpy((*distance), d_distance, nv*sizeof(int), cudaMemcpyDeviceToHost));
+	cudaCheck(cudaMemcpy((*previous), d_previous, nv*sizeof(int), cudaMemcpyDeviceToHost));
+
+
+	// check for negative cycles
+	int neg_cycle = false;
+
+	for (int v2 = 0; v2 < nv; v2++)
+	{
+		for (int e = (row_ptr)[v2]; e < (row_ptr)[v2+1]; e++)
+		{
+			int tempDistance = (*distance)[v2] + (weights)[e];
+			int neigh = (col_ind)[e];
+
+			if (tempDistance < (*distance)[neigh] && (*distance)[v2] != INT_MAX)
+			{
+				printf("GPU belman ford Error: negative cycle exists\n");
+				neg_cycle = true;
+				break;
+			}
+
+		}
+		if(neg_cycle)
+			break;
+	}
+
 
 	// Deallocation
-	cudaFree(d_row_ptr);
-	cudaFree(d_col_ind);
-	cudaFree(d_row_ind);
-	cudaFree(d_weights);
-	cudaFree(d_distance);
-	cudaFree(d_previous);
+	cudaCheck(cudaFree(d_row_ptr));
+	cudaCheck(cudaFree(d_col_ind));
+	cudaCheck(cudaFree(d_row_ind));
+	cudaCheck(cudaFree(d_weights));
+	cudaCheck(cudaFree(d_distance));
+	cudaCheck(cudaFree(d_previous));
 
 	printf("GPU SBF time: %f\n", elapsed/1000);
+
+	/*
+	for(int i = 0; i < nv; i++)
+	{
+		if((*distance)[i] != INT_MAX)
+			printf("%i\n", (*distance)[i]);
+
+	}
+	*/
+	
 }
