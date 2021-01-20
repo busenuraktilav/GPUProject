2348c82d96ad02ecd380f59af9548b4bde623bad
buse <busenuraktilav@gmail.com>
2021-01-20 12:45:44 +0300

    duplicate removal 64bit atomic instructions - BF update

--- GPU/bellman_ford.cu
+++ GPU/bellman_ford.cu
@@ -11,7 +11,7 @@ extern "C" {
 #include <cuda.h>
 #include <cuda_profiler_api.h>
 
-#define N_THREADS_PER_BLOCK 1024
+#define N_THREADS_PER_BLOCK 128
 
 
 #define cudaCheck(error) \
@@ -30,10 +30,7 @@ __global__ void relax(int *row_ptr, int *col_ind, int *weights, int *queue, int 
     if (tid < size) { 
         
     	u = queue[tid];
-    	//printf("queue[%i]: %i\n", tid, queue[tid]);
     	du = distance[u];
-
-    	//printf("row_ptr[%i]: %i, row_ptr[%i]: %i\n", u, row_ptr[u], u+1, row_ptr[u+1]);
 
         for (int e = row_ptr[u]; e < row_ptr[u + 1]; e++) {
             
@@ -41,13 +38,8 @@ __global__ void relax(int *row_ptr, int *col_ind, int *weights, int *queue, int 
             w = weights[e];
             dv = distance[v];
             add = du + w;
-            //printf("tid:%i, u: %i, du: %i, v: %i, w: %i, dv: %i, add:%i\n",tid, u, du, v, w, dv,add );
 
-            if (du == INT_MAX){
-                add = INT_MAX;
-            }
-
-            if (add < dv)
+            if (add < dv && du != INT_MAX)
             {
             	atomicMin(&distance[v],add);
             	index = atomicAdd(nextSize, 1);
@@ -57,21 +49,89 @@ __global__ void relax(int *row_ptr, int *col_ind, int *weights, int *queue, int 
         }
     }
 
-    if(tid == size)
-    {
+}
 
+__global__ void relaxAtom(int *row_ptr, int *col_ind, int *weights, int *queue, int *nextQueue, int size, int* nextSize, int2* distance, int *iter) 
+{
+
+    int index, u, v, w, du, dv, add, tid = threadIdx.x + (blockDim.x * blockIdx.x);
+
+    if (tid < size) { 
+        
+    	u = queue[tid];
+    	du = distance[u].x;
+
+        for (int e = row_ptr[u]; e < row_ptr[u + 1]; e++) {
+            
+            v = col_ind[e];
+            w = weights[e];
+            dv = distance[v].x;
+            add = du + w;
+
+            if (add < dv && du != INT_MAX)
+            {
+            	atomicMin(&distance[v].x,add);
+
+            	if(distance[v].y != *iter)
+            	{
+	            	atomicMax(&distance[v].y,*iter);
+	            	index = atomicAdd(nextSize, 1);
+					nextQueue[index] = v;
+				}
+            }
+        }
     }
 }
 
+/*
+__global__ void relaxInit(int *row_ptr, int *col_ind, int *weights, int *source, int *nextQueue, int* size, int* nextSize, int2* distance)
 
-int
-compare_ints (const void *a, const void *b)
 {
-  const int *da = (const int *) a;
-  const int *db = (const int *) b;
+    int index, v, w, du, tid = threadIdx.x + (blockDim.x * blockIdx.x);
 
-  return (int)(*da > *db) - (*da < *db);
+    if (tid < *size) { 
+        
+    	du = distance[*source].x;
+    	//printf("du %i\n", du);
+
+        for (int e = row_ptr[*source]; e < row_ptr[(*source) + 1]; e++) {
+            
+            v = col_ind[e];
+            w = weights[e];
+            atomicMin(&distance[v].x,du+w);
+
+            //if (distance[v].y != 1)
+            //{
+	        	
+	        	index = atomicAdd(nextSize, 1);
+				nextQueue[index] = v;
+
+				//printf("distance %i\n", v);
+			//}
+
+        }
+    }
+    __syncthreads();
+}*/
+
+
+
+__global__ void initVar(int2 *distance, int *nv)
+{
+	int bdim = blockDim.x, gdim = gridDim.x, bid = blockIdx.x, tid = threadIdx.x;
+	int i = bdim * bid + tid;
+	int skip = bdim * gdim;
+
+	for (int k = i; k < *nv; k += skip) {
+
+		distance[k].x = INT_MAX;
+		distance[k].y = 1; // dont update this in relaxinit. update here. no need for mem copy
+	}
+
+	__syncthreads();
+
 }
+
 
 
 extern "C"
@@ -79,95 +139,98 @@ extern "C"
 void sbf(const int *row_ptr, const int *col_ind, const int *row_ind, const int *weights, int **distance, int **previous, const int nv, const int ne, int source)
 {
 
-	// Initialize GPU variables
-	int *d_row_ptr, *d_col_ind, *d_row_ind, *d_weights, *d_distance, *d_previous, *d_nv, *d_ne;
-	int *d_queue, *d_nextQueue, *d_nextSize;
-	
-	// Initialize CPU variables
-	*distance = (int*)malloc(nv*sizeof(int)); 
-	*previous = (int*)malloc(nv*sizeof(int));
-	int *visited = (int *)calloc(nv, sizeof(int));
-
-
-	for (int i = 0; i < nv; i++)
-	{
-		(*distance)[i] = INT_MAX;
-		(*previous)[i] = -1;
-	}
-
-	(*distance)[source] = 0;
-	visited[source] = 1;
-
-
-	//To increase parallelism, first process the source vertex
-	int srcNeigh = row_ptr[source + 1] - row_ptr[source];
-	int *srcArr = (int*)calloc(srcNeigh, sizeof(int));
-	int index = 0;
-	(*distance)[source] = 0;
-
-	for (int i = row_ptr[source]; i < row_ptr[source + 1]; i++) { // for each neighbor of source node
-		
-		int w = weights[i];
-		int du = (*distance)[source];
-		int dv = (*distance)[col_ind[i]];
-
-		if (du + w < dv && du != INT_MAX)
-		{
-			(*distance)[col_ind[i]] = du + w;
-			srcArr[index++] = col_ind[i]; // add to frontier
-			//printf("col_ind[i]: %i\n", col_ind[i]);
-		}
-	}
-
-
-	// Allocate device
-	cudaCheck(cudaMalloc((void **)&d_row_ptr, (nv+1)*sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_col_ind, (ne+1)*sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_row_ind, (ne+1)*sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_weights, (ne+1)*sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_distance, nv*sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_previous, nv*sizeof(int)));
-	cudaCheck(cudaMalloc((void**) &d_nextSize, sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_nv, sizeof(int)));
-	cudaCheck(cudaMalloc((void **)&d_ne, sizeof(int)));
-	cudaCheck(cudaMalloc((void **) &d_queue, (nv+1)*sizeof(int)));
-    cudaCheck(cudaMalloc((void **) &d_nextQueue, (nv+1)*sizeof(int)));
-    cudaCheck(cudaMalloc((void **) &d_nextSize, sizeof(int)));
-
-	//Copy inputs to device
-	cudaCheck(cudaMemcpy(d_row_ptr, row_ptr, (nv+1)*sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_col_ind, col_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_row_ind, row_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_weights, weights, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_distance, (*distance), nv*sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_nv, &nv, sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_ne, &ne, sizeof(int), cudaMemcpyHostToDevice));
-	cudaCheck(cudaMemcpy(d_queue, srcArr, srcNeigh * sizeof(int), cudaMemcpyHostToDevice));
-
-
-	int size = srcNeigh;
-	int *nextSize = (int*)malloc(sizeof(int));
-	*nextSize = 0;
-	
 	cudaEvent_t start;
 	cudaEvent_t stop;
 	cudaCheck(cudaEventCreate(&start));
 	cudaCheck(cudaEventCreate(&stop));
 	cudaCheck(cudaEventRecord(start, 0));
 
-	int round = 1;
-
 	cudaProfilerStart();
 
+	// Initialize GPU variables
+	int *d_row_ptr, *d_col_ind, *d_weights, *d_nv;
+	int *d_queue, *d_nextQueue, *d_nextSize, *d_iter;
+	int2* d_dist;
+
+
+	int2* dist = (int2*)malloc(nv*sizeof(int2));
+
+
+	cudaCheck(cudaMalloc((void **)&d_row_ptr, (nv+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_col_ind, (ne+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_weights, (ne+1)*sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_dist, nv*sizeof(int2)));
+	cudaCheck(cudaMalloc((void **)&d_nv, sizeof(int)));
+
+	cudaCheck(cudaMemcpy(d_nv, &nv, sizeof(int), cudaMemcpyHostToDevice));
+
+
+	initVar<<<(nv + N_THREADS_PER_BLOCK - 1) / N_THREADS_PER_BLOCK, N_THREADS_PER_BLOCK>>>(d_dist, d_nv);
+
+	cudaCheck(cudaMemcpy(dist, d_dist, nv*sizeof(int2), cudaMemcpyDeviceToHost));
+
+	//To increase parallelism, first process the source vertex
+	int srcNeigh = row_ptr[source + 1] - row_ptr[source];
+	int *srcArr = (int*)calloc(srcNeigh, sizeof(int));
+	int index = 0;
+	dist[source].x = 0;
+
+	for (int i = row_ptr[source]; i < row_ptr[source + 1]; i++) { // for each neighbor of source node
+		
+		int w = weights[i];
+		int du = dist[source].x;
+		int dv = dist[col_ind[i]].x;
+
+		if (du + w < dv && du != INT_MAX)
+		{
+			dist[col_ind[i]].x = du + w;
+			dist[col_ind[i]].y = 1;
+			srcArr[index++] = col_ind[i]; // add to frontier
+		}
+	}
+
+	int *iter = (int*)malloc(sizeof(int));
+	*iter = 2;
+
+
+	// Allocate device
+	
+	cudaCheck(cudaMalloc((void **)&d_nextSize, sizeof(int)));
+	cudaCheck(cudaMalloc((void **)&d_queue, (nv+1)*sizeof(int)));
+    cudaCheck(cudaMalloc((void **)&d_nextQueue, (nv+1)*sizeof(int)));
+    cudaCheck(cudaMalloc((void **)&d_iter, sizeof(int)));
+
+	//Copy inputs to device
+	cudaCheck(cudaMemcpy(d_row_ptr, row_ptr, (nv+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_col_ind, col_ind, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_weights, weights, (ne+1)*sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_dist, dist, nv*sizeof(int2), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_queue, srcArr, srcNeigh * sizeof(int), cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_iter, iter, sizeof(int), cudaMemcpyHostToDevice));
+	
+
+
+	int size = srcNeigh;
+	int *nextSize = (int*)malloc(sizeof(int));
+	*nextSize = 0;
+	
+
+	int round = 1;
 	int temp = 0;
 
-	while((size > 0) && (round < nv)) { temp += size;
+
+	while((size > 0) && (round < nv) && temp < ne) { temp += size;
+
+		cudaCheck(cudaMemcpy(d_iter, iter, sizeof(int), cudaMemcpyHostToDevice));
 
 		cudaCheck(cudaMemcpy(d_nextSize, nextSize, sizeof(int), cudaMemcpyHostToDevice));
 
-		relax<<<(size + 1023) / 1024, 1024>>>(d_row_ptr, d_col_ind, d_weights, d_queue, d_nextQueue, size, d_nextSize, d_nv, d_distance);
+		relaxAtom<<<(size + N_THREADS_PER_BLOCK - 1) / N_THREADS_PER_BLOCK, N_THREADS_PER_BLOCK>>>(d_row_ptr, d_col_ind, d_weights, d_queue, d_nextQueue, size, d_nextSize, d_dist, d_iter);
 
 		cudaCheck(cudaMemcpy(nextSize, d_nextSize, sizeof(int), cudaMemcpyDeviceToHost));
+
+		(*iter) ++;
+
 
 		printf("size: %i\n", size);
 
@@ -183,16 +246,15 @@ void sbf(const int *row_ptr, const int *col_ind, const int *row_ind, const int *
 
 	printf("total size: %i\n", temp);
 
+	// Copy outputs to host
+	cudaCheck(cudaMemcpy(dist, d_dist, nv*sizeof(int2), cudaMemcpyDeviceToHost));
+	
 	cudaProfilerStop();
 
 	cudaCheck(cudaEventRecord(stop, 0));
 	cudaCheck(cudaEventSynchronize(stop));
 	float elapsed;
 	cudaCheck(cudaEventElapsedTime(&elapsed, start, stop));
-
-	// Copy outputs to host
-	cudaCheck(cudaMemcpy((*distance), d_distance, nv*sizeof(int), cudaMemcpyDeviceToHost));
-	cudaCheck(cudaMemcpy((*previous), d_previous, nv*sizeof(int), cudaMemcpyDeviceToHost));
 
 
 	// check for negative cycles
@@ -202,10 +264,10 @@ void sbf(const int *row_ptr, const int *col_ind, const int *row_ind, const int *
 	{
 		for (int e = (row_ptr)[v2]; e < (row_ptr)[v2+1]; e++)
 		{
-			int tempDistance = (*distance)[v2] + (weights)[e];
+			int tempDistance = dist[v2].x + (weights)[e];
 			int neigh = (col_ind)[e];
 
-			if (tempDistance < (*distance)[neigh] && (*distance)[v2] != INT_MAX)
+			if (tempDistance < dist[neigh].x && dist[v2].x != INT_MAX)
 			{
 				printf("GPU belman ford Error: negative cycle exists\n");
 				neg_cycle = true;
@@ -221,20 +283,11 @@ void sbf(const int *row_ptr, const int *col_ind, const int *row_ind, const int *
 	// Deallocation
 	cudaCheck(cudaFree(d_row_ptr));
 	cudaCheck(cudaFree(d_col_ind));
-	cudaCheck(cudaFree(d_row_ind));
 	cudaCheck(cudaFree(d_weights));
-	cudaCheck(cudaFree(d_distance));
-	cudaCheck(cudaFree(d_previous));
+	cudaCheck(cudaFree(d_dist));
 
-	printf("GPU SBF time: %f\n", elapsed/1000);
 
-	/*
-	for(int i = 0; i < nv; i++)
-	{
-		if((*distance)[i] != INT_MAX)
-			printf("%i\n", (*distance)[i]);
 
-	}
-	*/
+	printf("GPU SBF time(ms): %f\n", elapsed);
 	
 }
