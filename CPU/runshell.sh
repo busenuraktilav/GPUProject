e130e3be0a076b529d22535bb8b9e2962bc13ca4
buse <busenuraktilav@gmail.com>
2021-05-26 10:28:40 +0300

    bug is fixed on bash file

--- CPU/runshell.sh
+++ CPU/runshell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "vertexNum,edgeNum,iterationNum,maxEdgeDegree,minProcessEdge,percentage,sOriginalDistance,sMinEdgetoProcess,sApprAttrValues,sReduceExecution,sPartialGraphProcess,sAtomicMinBlock,sAtomicMaxBlock,sAtomicAddBlock,sAtomicExchBlock,Error,executionTime" >> '../bellman_performance_results.csv'
+echo "vertexNum,edgeNum,iterationNum,maxEdgeDegree,minProcessEdge,percentage,sOriginalDistance,sMinEdgetoProcess,sApprAttrValues,sReduceExecution,sPartialGraphProcess,sAtomicMinBlock,sAtomicMaxBlock,sAtomicAddBlock,Error,executionTime" >> '../bellman_performance_results.csv'
 
 file1='commands_bellman.txt'
 file2='graph_names.txt'
@@ -41,7 +41,7 @@ iter=$(head -n 1 $file3)
 
 if [ "$line2" == "0 1 0 0 0 0 0 0 0" ];
 then
-for t in {1..17};
+for t in {1..16};
 do
 for n in {0..9};
 do
@@ -78,45 +78,42 @@ done
 fi
 
 
-p=(0.6, 0.5)
-
 if [ "$line2" == "0 0 0 0 1 0 0 0 0" ];
 then
-for t in {0..1}
+for n in {0..9}
 do
-for ((n=1; n<=10; n++))
-do
-./run $line2 $line1 0 $iter ${p[t]} 0 1
+./run $line2 $line1 0 $iter 0.6 0 1
 done
-./run $line2 $line1 0 $iter ${p[t]} 1 1
+./run $line2 $line1 0 $iter 0.6 1 1
 rm 'time_results.txt'
-done
 fi
 
 
 if [ "$line2" == "0 0 0 0 0 1 0 0 0" ];
 then
-for t in {0..8}
+for n in {0..9}
 do
 ./run $line2 $line1 0 $iter 0 0 1
 done
 ./run $line2 $line1 0 $iter 0 1 1
 rm 'time_results.txt'
 fi
+
 
 if [ "$line2" == "0 0 0 0 0 0 1 0 0" ];
 then
-for t in {0..8}
+for n in {0..9}
 do
 ./run $line2 $line1 0 $iter 0 0 1
 done
 ./run $line2 $line1 0 $iter 0 1 1
 rm 'time_results.txt'
 fi
+
 
 if [ "$line2" == "0 0 0 0 0 0 0 1 0" ];
 then
-for t in {0..8}
+for n in {0..9}
 do
 ./run $line2 $line1 0 $iter 0 0 1
 done
@@ -124,11 +121,70 @@ done
 rm 'time_results.txt'
 fi
 
-done < $file1
-done < $file2
+
+if [ "$line2" == "0 1 0 1 0 0 0 0 0" ];
+then
+for i in {1..16};
+do
+for t in $(seq 0 $iter);
+do
+for n in {0..9};
+do
+./run $line2 $line1 $i $t 1 0 1
+done
+./run $line2 $line1 $i $t 1 1 1
+rm 'time_results.txt'
+done
+done
+fi
 
 
-echo "vertexNum,edgeNum,iterationNum,maxEdgeDegree,minProcessEdge,percentage,sOriginalDistance,sMinEdgetoProcess,sApprAttrValues,sReduceExecution,sPartialGraphProcess,sAtomicMinBlock,sAtomicMaxBlock,sAtomicAddBlock,sAtomicExchBlock,Error,executionTime" >> '../dijkstra_performance_results.csv'
+if [ "$line2" == "0 1 0 0 1 0 0 0 0" ];
+then
+for i in {1..16};
+do
+for n in {0..9};
+do
+./run $line2 $line1 $i $iter 0.6 0 1
+done
+./run $line2 $line1 $i $iter 0.6 1 1
+rm 'time_results.txt'
+done
+fi
+
+
+if [ "$line2" == "0 0 0 1 1 0 0 0 0" ];
+then
+for t in $(seq 0 $iter);
+do
+for n in {0..9};
+do
+./run $line2 $line1 0 $t 0.6 0 1
+done
+./run $line2 $line1 0 $t 0.6 1 1
+rm 'time_results.txt'
+done
+fi
+
+
+if [ "$line2" == "0 1 0 1 1 0 0 0 0" ];
+then
+for i in {1..16};
+do
+for t in $(seq 0 $iter);
+do
+for n in {0..9};
+do
+./run $line2 $line1 $i $t 0.6 0 1
+done
+./run $line2 $line1 $i $t 0.6 1 1
+rm 'time_results.txt'
+done
+done
+fi
+
+
+echo "vertexNum,edgeNum,iterationNum,maxEdgeDegree,minProcessEdge,percentage,sOriginalDistance,sMinEdgetoProcess,sApprAttrValues,sReduceExecution,sPartialGraphProcess,sAtomicMinBlock,sAtomicMaxBlock,sAtomicAddBlock,Error,executionTime" >> '../dijkstra_performance_results.csv'
 
 
 file1='commands_dijkstra.txt'
@@ -168,10 +224,9 @@ fi
 iter=$(head -n 1 $file3)
 
 
-
 if [ "$line2" == "0 1 0 0 0 0 0 0 0" ];
 then
-for t in {1..17};
+for t in {1..16};
 do
 for n in {0..9};
 do
@@ -219,12 +274,28 @@ rm 'time_results.txt'
 fi
 
 
+if [ "$line2" == "0 1 0 1 0 0 0 0 0" ];
+then
+for i in {1..16};
+do
+for t in $(seq 0 $iter);
+do
+for n in {0..9};
+do
+./run $line2 $line1 $i $t 1 0 2
+done
+./run $line2 $line1 $i $t 1 1 2
+rm 'time_results.txt'
+done
+done
+fi
+
+
 done < $file1
 done < $file2
 
 
-
-echo "vertexNum,edgeNum,iterationNum,maxEdgeDegree,minProcessEdge,percentage,sOriginalDistance,sMinEdgetoProcess,sApprAttrValues,sReduceExecution,sPartialGraphProcess,sAtomicMinBlock,sAtomicMaxBlock,sAtomicAddBlock,sAtomicExchBlock,Error,executionTime" >> '../hybrid_performance_results.csv'
+echo "vertexNum,edgeNum,iterationNum,maxEdgeDegree,minProcessEdge,percentage,sOriginalDistance,sMinEdgetoProcess,sApprAttrValues,sReduceExecution,sPartialGraphProcess,sAtomicMinBlock,sAtomicMaxBlock,sAtomicAddBlock,Error,executionTime" >> '../hybrid_performance_results.csv'
 
 
 file1='commands_dijkstra.txt'
@@ -263,12 +334,10 @@ fi
 
 iter=$(head -n 1 $file3)
 
-echo 'iter: ' $iter
-
 
 if [ "$line2" == "0 1 0 0 0 0 0 0 0" ];
 then
-for t in {1..17};
+for t in {1..16};
 do
 for n in {0..9};
 do
@@ -304,6 +373,7 @@ rm 'time_results.txt'
 done
 fi
 
+
 if [ "$line2" == "0 0 0 0 0 0 0 0 0 1" ];
 then
 for n in {0..9};
@@ -312,6 +382,23 @@ do
 done
 ./run $line2 $line1 0 $iter 1 1 3
 rm 'time_results.txt'
+fi
+
+
+if [ "$line2" == "0 1 0 1 0 0 0 0 0" ];
+then
+for i in {1..16};
+do
+for t in $(seq 0 $iter);
+do
+for n in {0..9};
+do
+./run $line2 $line1 $i $t 1 0 3
+done
+./run $line2 $line1 $i $t 1 1 3
+rm 'time_results.txt'
+done
+done
 fi
 
 
